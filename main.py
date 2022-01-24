c7e820ab13b8243cddf9179dbb462e4792a61515
doodko <alexdoodko@gmail.com>
2022-01-24 22:34:07 +0200

    Add main logic and frontend form

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import Response
 import uvicorn
+import db
 
 
 app = FastAPI()
@@ -8,7 +9,21 @@ app = FastAPI()
 
 @app.get("/")
 def index_page():
-    return Response("Let's test this shit", media_type="text/html")
+    with open('templates/index.html', 'r', encoding='utf-8') as f:
+        home_page = f.read()
+    return Response(home_page, media_type="text/html")
+    
+
+@app.post('/hello')
+def say_hello(username : str = Form(...)):
+    answer = db.check_if_met_before(username)
+    return Response(answer, media_type='text/html')
+
+
+@app.get("/friends")
+def friends_page():
+    friens_list = 'Тут були: ' + ', '.join(db.find_all_friends()) + '...'
+    return Response(friens_list, media_type="text/html")
     
 
 if __name__ == "__main__":
